1fa34c9319b31860337db0c706eac2711249eade
Yuryhutyrchyk <yury.hutyrchyk@gmail.com>
2016-07-08 13:05:39 +0200

    Target 9 , the addition of

--- src/lesson3/Task9.java
+++ src/lesson3/Task9.java
@@ -12,9 +12,25 @@ public class Task9 {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		
-
+	public static void main(String[] args) { // Method rearranges all the zeros
+												// at the end of the array
+		int[] arr = { 0, 3, 4, 0, 7, 9, 0, 3, 7, 5 }; // array initialization
+		int size = arr.length;
+		System.out.print("Первичный вид массива: ");
+		for (int i = 0; i < size; i++) {
+			System.out.printf("%2d", arr[i]);
+		}
+		for (int i = 0; i < size; i++) {
+			if (arr[i] == 0) {
+				for (int j = i; j < size - 1; j++) {
+					arr[j] = arr[j + 1];
+				}
+				arr[size - 1] = 0;
+			}
+		}
+		System.out.print("\nВид массива после редактирования: ");
+		for (int i = 0; i < size; i++) {
+			System.out.printf("%2d", arr[i]);
+		}
 	}
-
 }
