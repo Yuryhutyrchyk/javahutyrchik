477da26312255c095677916bb29aee6aecd30f23
Yuryhutyrchyk <yury.hutyrchyk@gmail.com>
2016-07-12 15:05:25 +0200

    update

--- src/lesson4/taskC.java
+++ src/lesson4/taskC.java
@@ -16,8 +16,7 @@ public class TaskC {
 												// symbols except spaces , which
 												// are not letters
 		String str = "***Now!¹¹ %every%thing? i*s ¹all¹ *r*i*g*h*t*!!!";
-		char[] ch = str.toCharArray(); // It creates an array of characters from
-										// string
+		System.out.println(str);
 		String s = str.replaceAll("(?u)[^\\pL ]", ""); // removes all but the
 														// gaps and letters
 		System.out.println(s);
